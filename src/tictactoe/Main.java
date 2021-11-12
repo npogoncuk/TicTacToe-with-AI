98057c6ab4af24d91257b60988f3d56b44b0d43e
npogoncuk <npogoncuk@gmail.com>
2021-11-12 22:10:40 +0300

    added comment into Main.java

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -5,6 +5,8 @@ import tictactoe.game.GameStart;
 public class Main {
 
     public static void main(String[] args) {
+        // method start() takes boolean parameter
+        // if it is true, then user will be able to see rules and information about the game
         GameStart.start(true);
     }
 }
